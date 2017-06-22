b5bd58c15bcdbe72ccbfb15d858298460a5ffdf3
Снежинка <>
2017-06-22 15:59:37 +0700

    rename ConsoleApplication1(FilesConverter)

---
+++ Project Test task/ConsoleApplication1/FilesConverter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApplication1
+{
+        /*[StructLayout(LayoutKind.Sequential, Pack = 1)]*/
+        public struct TradeRecord
+        {
+            public int id;
+            public int account;
+            public double volume;
+            /*[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]*/
+            public string comment;
+
+            public TradeRecord (int a, int b, double c, string d)
+            {
+                id = a;
+                account = b;
+                volume = c;
+                comment = d;
+            }
+        } 
+        
+        class Program
+        { 
+            static void Main(string[] args)
+        {
+            TradeRecord traderecord = new TradeRecord ( 1, 2, 3.5, "good" );
+            Console.WriteLine("Укажите путь для создаваемого бинарного файла (пример: D/Project/..) :");
+            try
+            {
+                string path = Console.ReadLine();
+                Console.WriteLine(path);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+
+        }
+}
